4c2f1aa5bfeb56ab124a32b9c9b1f076da8a8fd6
Maskayop <alexei.erepov@gmail.com>
2026-07-16 19:37:46 +0300

    Настройки вращения суппорта вместе с колесом

---
+++ Assets/_code/Tractor/Follower.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Tractor
+{
+    public class Follower : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
